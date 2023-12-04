129d3b1322aeb8defb41d301758f7a6de1bbdaf7
Bar Cohen <sscohen99@gmail.com>
2023-12-04 12:23:56 +0200

    added mongodb user model middlewares

--- db/userModel.js
+++ db/userModel.js
@@ -56,6 +56,44 @@ const userSchema = new Schema({
   }
 })
 
+//region Middlewares
+/* validate password */
+userSchema.pre(/(save|update)/, {
+  document: true,
+  query: false
+}, function(next) {
+  if (!this.isModified('password')) return next()
+  if (!this.passwordConfirm) return next(new AppError(400, 'Validation error: password confirm is required'))
+  if (this.password !== this.passwordConfirm) {
+    return next(new AppError(400, 'Validation error: Passwords must be the same'))
+  }
+
+  next()
+})
+
+/* encrypt password */
+userSchema.pre(/(save|update)/, {
+  document: true,
+  query: false
+}, async function(next) {
+  if (!this.isModified('password')) return next()
+
+  const salt = await bcrypt.genSalt(Number(process.env.BCRYPT_SALT))
+  this.password = await bcrypt.hash(this.password, salt)
+
+  this.passwordConfirm = undefined
+  this.passwordLastChangeAt = Date.now()
+
+  next()
+})
+
+/* filter deleted users */
+userSchema.pre(/^find/, function(next) {
+  this.find({ active: { $ne: false } })
+  next()
+})
+//endregion
+
 const User = model('User', userSchema)
 
 module.exports = User
