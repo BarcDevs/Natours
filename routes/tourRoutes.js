7d9a4c8040e7c71f6fb2e436bc81a27904693ad3
Bar Cohen <sscohen99@gmail.com>
2023-12-04 12:19:20 +0200

    added tour router

---
+++ routes/tourRoutes.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const {
+  aliasTopTours,
+  getTours,
+  createTour,
+  deleteTour,
+  getTourById,
+  updateTour,
+  getToursStats,
+  getMonthlyPlan
+} = require('../controllers/tourController')
+const { routes: r } = require('../config')
+const {
+  protectRoute,
+  restrictRoute
+} = require('../controllers/authController')
+
+const routes = r.toursRoutes
+const router = express.Router()
+
+router
+  .route('/')
+  .get(
+    protectRoute,
+    getTours
+  )
+  .post(
+    protectRoute,
+    restrictRoute('admin', 'lead-guide'),
+    createTour
+  )
+
+router
+  .route(routes.top5)
+  .get(aliasTopTours, getTours)
+
+router
+  .route(routes.stats)
+  .get(getToursStats)
+
+router
+  .route(`${routes.monthlyPlan}/:year`)
+  .get(
+    protectRoute,
+    restrictRoute('admin', 'lead-guide', 'guide'),
+    getMonthlyPlan
+  )
+
+router
+  .route('/:id')
+  .get(protectRoute, getTourById)
+  .patch(
+    protectRoute,
+    restrictRoute('admin', 'lead-guide'),
+    updateTour
+  )
+  .delete(
+    protectRoute,
+    restrictRoute('admin', 'lead-guide'),
+    deleteTour
+  )
+
+module.exports = router
