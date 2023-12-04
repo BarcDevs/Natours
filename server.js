17ffab2a92a5f59d9c2e6e677cf43937fe45fb10
Bar Cohen <sscohen99@gmail.com>
2023-12-04 12:09:12 +0200

    created server

---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv')
+  .config()
+const app = require('./app')
+const db = require('./db')
+
+process.on('uncaughtException', err => {
+  console.error(`${err.name}: ${err.message}
+Shutting down due to an error...`)
+  process.exit(1)
+})
+
+const port = process.env.PORT || 3000
+
+const server = app.listen(port, () => {
+  console.log(`Server listening at port ${port}`)
+})
+
+process.on('unhandledRejection', err => {
+  console.error(`${err.name}: ${err.message}
+Shutting down due to an error...`)
+  server.close(() => {
+    process.exit(1)
+  })
+})
