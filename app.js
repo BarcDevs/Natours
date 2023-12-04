8e92f1e3a70bb92e8513cbbe85c9b0623c0bb7f8
Bar Cohen <sscohen99@gmail.com>
2023-12-04 12:17:22 +0200

    added express middlewares

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 //region REQUIRE
 const express = require('express')
+const rateLimit = require('express-rate-limit')
+const helmet = require('helmet')
+const mongoSanitize = require('express-mongo-sanitize')
+const { xss } = require('express-xss-sanitizer')
+const morgan = require('morgan')
+const hpp = require('hpp')
 const config = require('./config')
 const { tourRouter } = require('./routes')
 const { userRouter } = require('./routes')
@@ -12,6 +18,32 @@ const {
 //region CONFIG
 const app = express()
 const { endpoints } = config
+//endregion
+
+//region MIDDLEWARE
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+const limiter = rateLimit({
+  max: process.env.MAX_REQUEST_LIMIT,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests. try again later'
+})
+
+app.use(helmet())
+app.use(xss({
+  allowedKeys: ['name']
+}))
+app.use(mongoSanitize())
+app.use('/api', limiter)
+app.use(express.json({
+  limit: '10kb'
+}))
+app.use(express.static(`${__dirname}/public`))
+app.use(hpp({
+  whitelist: ['duration', 'ratingsAverage', 'price', 'ratingsQuantity', 'maxGroupSize', 'priceDiscount', 'difficulty']
+}))
 //endregion
 
 //region ROUTES
