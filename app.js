dd81fdf07915b474b751f91f286041e26eb3c880
Bar Cohen <sscohen99@gmail.com>
2023-12-04 12:16:12 +0200

    added routers to the app

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 //region REQUIRE
 const express = require('express')
 const config = require('./config')
+const { tourRouter } = require('./routes')
+const { userRouter } = require('./routes')
+const {
+  notFound,
+  errorHandler
+} = require('./controllers/errorController')
 //endregion
 
 //region CONFIG
@@ -8,4 +14,12 @@ const app = express()
 const { endpoints } = config
 //endregion
 
+//region ROUTES
+app.use(endpoints.toursEndpoint, tourRouter)
+app.use(endpoints.usersEndpoint, userRouter)
+
+// not found handler
+app.all('*', notFound)
+
+app.use(errorHandler)
 module.exports = app
