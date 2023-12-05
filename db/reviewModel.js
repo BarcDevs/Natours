93cc60f35ba67b5adf694b7cfff03b42d7cab29d
Bar Cohen <sscohen99@gmail.com>
2023-12-05 11:37:28 +0200

    added review schema

---
+++ db/reviewModel.js
@@ -0,0 +1,41 @@
+const {
+  Schema,
+  model
+} = require('mongoose')
+
+const reviewSchema = new Schema({
+  review: {
+    type: String,
+    required: [true, 'Review can not be empty']
+  },
+  rating: {
+    type: Number,
+    required: [true, 'Rating is required'],
+    validate: {
+      validator: rating => (!(rating < 1) && !(rating > 5)),
+      message: 'rating must be between 1 and 5'
+    }
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  tour: {
+    type: Schema.ObjectId,
+    ref: 'Tour',
+    required: [true, 'Review must belong to a tour']
+  },
+  author: {
+    type: Schema.ObjectId,
+    ref: 'User',
+    required: [true, 'Review must belong to an author']
+  }
+},
+{
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true }
+})
+
+const ReviewSchema = model('ReviewSchema', reviewSchema)
+
+module.exports = ReviewSchema
