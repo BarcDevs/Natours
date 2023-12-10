aebc8fc7aec3e801702971dc2043e6dc2abbe227
Bar Cohen <sscohen99@gmail.com>
2023-12-10 14:39:24 +0200

    simplified render controller helper function

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,27 +1,25 @@
 const { catchAsync } = require('./errorController')
 const Tour = require('../db/tourModel')
 
-const render = (res, path, data = {}) => res.status(200)
-  .render(path, data)
+const render = (res, path, title, data = {}) => res.status(200)
+  .render(path, {
+    title,
+    ...data
+  })
 
 exports.renderOverview = catchAsync(async (req, res, next) => {
   const tours = await Tour.find()
-  render(res, 'overview', {
-    title: 'All Tours',
-    tours
-  })
+  render(res, 'overview', 'All Tours', { tours })
 })
 
 exports.renderTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findOne({
     slug: req.params.tourSlug
-  }).populate({
-    path: 'reviews',
-    fields: 'review rating author'
   })
+    .populate({
+      path: 'reviews',
+      fields: 'review rating author'
+    })
 
-  render(res, 'tour', {
-    title: `${tour.name} Tour`,
-    tour
-  })
+  render(res, 'tour', `${tour.name} Tour`, { tour })
 })
