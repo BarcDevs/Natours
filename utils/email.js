b91c8f0faf16613d7a7f804a997a6470fc6bf7e9
Bar Cohen <sscohen99@gmail.com>
2023-12-13 11:47:52 +0200

    added mail sender class

--- utils/email.js
+++ utils/email.js
@@ -1,24 +1,55 @@
 const nodemailer = require('nodemailer')
+const { renderFile } = require('pug')
+const htmlToText = require('html-to-text')
 
-const sendMail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD
-    }
-  })
-
-  const mailOptions = {
-    from: 'Bar Cohen <bar@gmail.com>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-    // html
+class Email {
+  constructor(user, url) {
+    this.to = user.email
+    this.name = user.name
+    this.url = url
+    this.from = `${process.env.EMAIL_FROM_NAME} <${process.env.EMAIL_FROM_ADDRESS}>`
   }
 
-  await transporter.sendMail(mailOptions)
+  createTransport() {
+    if (process.env.NODE_ENV === 'development') {
+      return nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+          user: process.env.EMAIL_USERNAME,
+          pass: process.env.EMAIL_PASSWORD
+        }
+      })
+    }
+    // email with sendgrid
+  }
+
+  async send(template, subject) {
+    const html = renderFile(`${__dirname}/../views/email/${template}.pug`, {
+      firstName: this.name.split(' ')[0],
+      url: this.url,
+      subject
+    })
+
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.convert(html)
+    }
+
+    await this.createTransport()
+      .sendMail(mailOptions)
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'welcome to the Natours Family!')
+  }
+
+  async sendResetPassword() {
+    await this.send('passwordReset', 'Your password reset token')
+  }
 }
 
-module.exports = sendMail
+module.exports = Email
